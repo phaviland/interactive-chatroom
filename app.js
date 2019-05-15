fc7099b7a863552d05fe37adba3ee8a6803c489f
phaviland <25873346+phaviland@users.noreply.github.com>
2019-05-15 19:52:53 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var map = new Map();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function () {
+});
+
+io.on('connection', function (socket) {
+    var name;
+    var timeout;
+
+    socket.on('signin', function (square, fn) {
+        if (map.has(square.name) || name !== undefined)
+            fn("Name already in use");
+        else {
+            name = square.name;
+            map.set(name, square);
+            fn(true);
+        }
+    });
+
+    socket.on('move', function (x, y) {
+        var square = map.get(name);
+        square.x = x;
+        square.y = y;
+    });
+
+    socket.on('sendmessage', function (message) {
+        message = message.trim();
+        if (message.length != 0) {
+            var square = map.get(name);
+            square.message = message;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () { square.message = ''; }, 5000);
+        }
+    });
+
+    socket.on('disconnect', function () {
+        map.delete(name);
+    });
+});
+
+setInterval(function () {
+    io.emit('update', Array.from(map.values()));
+}, 10);
---
+++ index.js
@@ -0,0 +1,79 @@
+﻿var socket = io('http://localhost:3000/');
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
+
+var square;
+var squares = new Array();
+
+window.requestAnimationFrame(animate);
+
+socket.on('update', function (squaresList) {
+    squares = squaresList;
+});
+
+function init(name) {
+    square = { x: Math.floor(Math.random() * 790), y: Math.floor(Math.random() * 590), name: name, message: '' };
+
+    socket.emit('signin', square, function (data) {
+        if (data === true) {
+            document.getElementById("nameForm").style.display = "none";
+            document.getElementById("errorMessage").style.display = "none";
+            document.getElementById("messageForm").style.display = "block";
+        } else {
+            document.getElementById('errorMessage').textContent = data;
+        }
+
+    });
+}
+
+function sendMessage(message) {
+    socket.emit('sendmessage', message);
+    document.getElementById('message').value = '';
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    ctx.beginPath();
+    ctx.clearRect(0, 0, 800, 600);
+
+    squares.forEach((obj, i) => {
+        ctx.rect(obj.x, obj.y, 10, 10);
+        ctx.font = '16px Arial';
+        ctx.fillText(obj.name, obj.x, obj.y + 25);
+        if (obj.message !== '')
+            ctx.fillText(obj.message, obj.x, obj.y - 5);
+    });
+
+    ctx.closePath();
+    ctx.stroke();
+}
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+
+    if (square === undefined)
+        return;
+
+    if (keyName === 'ArrowUp') {
+        if (square.y > 10) {
+            square.y = square.y - 10;
+            socket.emit('move', square.x, square.y);
+        }
+    } else if (keyName === 'ArrowDown') {
+        if (square.y < 590) {
+            square.y = square.y + 10;
+            socket.emit('move', square.x, square.y);
+        }
+    } else if (keyName === 'ArrowLeft') {
+        if (square.x > 10) {
+            square.x = square.x - 10;
+            socket.emit('move', square.x, square.y);
+        }
+    } else if (keyName === 'ArrowRight') {
+        if (square.x < 790) {
+            square.x = square.x + 10;
+            socket.emit('move', square.x, square.y);
+        }
+    }
+}, false);
